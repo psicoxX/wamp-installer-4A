3959bff3df696b4ba95517b0d6e1394f1bbab961
psicoxX <gonzalooficial110@gmail.com>
2024-03-01 09:55:50 -0600

    Create index.php
    wamp installer for windows 11

---
+++ practica2/index.php
@@ -0,0 +1,20 @@
+ <!-- GONZALO FELCIANO GARCIA MUNOZ 4A PROGRAMACION T/V 14/02/24-->
+<?php
+ $numero1 = 1;
+ $numero2= 2;
+ $suma = $numero1 + $numero2 ;
+ $resta = $numero2 - $numero1;
+
+ ?>
+ <script>
+    var numero1 = <?= $numero1 ?>;
+    var numero2 = <?= $numero2 ?>;
+    var suma = <?= $suma ?>;
+    var resta=<?= $resta?>;
+  
+    console.log('numero1: ' + numero1);
+    console.log('numero2: ' + numero2);
+    console.log('Suma: ' + suma);
+    console.log( 'Resta :' + resta);
+    console.log(' que inda papuuus');
+ </script>
